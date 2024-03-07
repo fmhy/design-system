550ad9d4b7881a817023d822d26e30c2769371c3
taskylizard <75871323+taskylizard@users.noreply.github.com>
2024-03-07 14:51:53 +0000

    docs: setup unocss

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from 'vitepress'
-
+import { defineConfig } from "vitepress";
+import UnoCSS from "unocss/vite";
+import { presetUno, presetWind, presetTypography } from "unocss";
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Design System",
   description: "🎋",
+  vite: {
+    plugins: [
+      UnoCSS({ presets: [presetUno(), presetWind(), presetTypography()] }),
+    ],
+  },
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
